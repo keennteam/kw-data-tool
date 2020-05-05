fc4d6ec4bbaaddd6b7cd360510fe5ac0176cd0a3
bitfede <f.g.defaveri@gmail.com>
2020-05-04 23:02:01 -0400

    Parses args into a python dictionary

--- kw-easy-data.py
+++ kw-easy-data.py
@@ -8,6 +8,6 @@ import json
 parser = argparse.ArgumentParser(description='Get suggestions and search volume for a keyword')
 parser.add_argument('-k', '--keyword', required=True, metavar='Keyword', help='The keyword that you want to get search data about')
 
-args = parser.parse_args()
+args = vars(parser.parse_args())
 
 print(args)
